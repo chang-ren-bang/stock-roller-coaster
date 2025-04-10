b547f066d6b9d487ac26e4afbe8547eeb010d387
Ben Chang <ben.chang@onelab.tw>
2025-04-10 10:36:35 +0800

    feat: 更新three.js模組導入路徑，使用本地檔案以提高加載效率

---
+++ libs/three.module.js
@@ -0,0 +1,5 @@
+// three.js ES module 版本
+// 請至 https://unpkg.com/three@latest/build/three.module.js 下載最新版本覆蓋此檔案
+// 以下為示意，請替換為完整內容
+
+export * from 'https://unpkg.com/three@latest/build/three.module.js';
